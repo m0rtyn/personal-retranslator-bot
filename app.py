88e27c9e800e418a15825d8872aa226e9292ac23
Martyn <zogacc@gmail.com>
2020-12-03 22:27:35 +0200

    update app

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-import re
-
 import telegram
 from flask import Flask, request
 
@@ -21,36 +19,18 @@ def respond():
   msg_id = update.message.message_id
 
   # Telegram understands UTF-8, so encode text for unicode compatibility
-  text = update.message.text.encode('utf-8').decode()
+  text = update.message.text.encode('utf-8')
   # for debugging purposes only
   print("got text message :", text)
-  # the first time you chat with the bot AKA the welcoming message
-  if text == "/start":
-    # print the welcoming message
-    bot_welcome = "Welcome to martyn retranslator bot."
-    # send the welcoming message
-    bot.sendMessage(chat_id=chat_id, text=bot_welcome, reply_to_message_id=msg_id)
+  
+  response = get_response(text)
+  bot.sendMessage(chat_id=chat_id, text=response, reply_to_message_id=msg_id)
 
+  return 'ok'
 
-  else:
-    try:
-      # clear the message we got from any non alphabets
-      text = re.sub(r"\W", "_", text)
-      # create the api link for the avatar based on http://avatars.adorable.io/
-      #  url = "https://api.adorable.io/avatars/285/{}.png".format(text.strip())
-      message = "message"
-      # reply with a photo to the name the user sent,
-      # note that you can send photos by url and telegram will fetch it for you
-      bot.sendMessage(chat_id=chat_id,text=message, reply_to_message_id=msg_id)
-    except Exception:
-      # if things went wrong
-      bot.sendMessage(chat_id=chat_id, text="There was a problem in thename you used, please enter different name",reply_to_message_id=msg_id)
-
-    return 'ok'
-
-@app.route('/setWebhook', methods=['GET', 'POST'])
+@app.route('/set_webhook', methods=['GET', 'POST'])
 def set_webhook():
-   s = bot.setWebhook('{URL}{HOOK}'.format(URL=URL, HOOK=TOKEN))
+   s = bot.setWebhook('https://{URL}/{HOOK}'.format(URL=URL, HOOK=TOKEN))
    if s:
        return "webhook setup ok"
    else:
@@ -62,4 +42,4 @@ def index():
 
 
 if __name__ == '__main__':
-   app.run(threaded=True)
+  app.run(threaded=True)
