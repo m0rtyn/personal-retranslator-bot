a8a482a016030ca970f5d4e4ea772354a697ce20
Martyn <zogacc@gmail.com>
2020-12-03 21:33:57 +0200

    set url

--- telebot/credentials.py
+++ telebot/credentials.py
@@ -1,3 +1,3 @@
 bot_token = "1427261129:AAEHCboM0MC81MI5qYv4t2lg6phVcBYXTL4"
 bot_user_name = "martyn_retranslator_bot"
-URL = "the heroku app link that we will create later"
+URL = "https://retranslator-bot.herokuapp.com/"
