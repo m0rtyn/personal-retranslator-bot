0e3a8bb959c8ba96e1fb2b1f57260690659d8acd
Martyn <zogacc@gmail.com>
2020-12-06 22:24:31 +0200

    debug bot

--- app.py
+++ app.py
@@ -48,15 +48,15 @@ def choice(update: Update, context: CallbackContext) -> None:
 
         # return SEND
 
-        chat_id = user_data.chat_id
-        channel_id = user_data.channel_id
-        message_id = user_data.message_id
+        # chat_id = user_data.chat_id
+        # channel_id = user_data.channel_id
+        # message_id = user_data.message_id
 
-        if chat_id != 129482161: # id of personal chat with bot
+        if user_data.chat_id != 129482161: # id of personal chat with bot
             return
 
-        updater.bot.forwardMessage(chat_id=chat_id, from_chat_id=channel_id, message_id=message_id)
-        
+        updater.bot.forwardMessage(chat_id=chat_id, from_chat_id=user_data.channel_id, message_id=user_data.message_id)
+
     except Exception as e:
       if hasattr(e, 'message'):
           print(e.message)
