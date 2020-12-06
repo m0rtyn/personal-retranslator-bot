872916113b6406c45fbc95a9e96400b6eee9b509
Martyn <zogacc@gmail.com>
2020-12-06 20:53:58 +0200

    update bot

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import logging
 import os
 
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram import (InlineKeyboardButton, InlineKeyboardMarkup,
+                      ReplyKeyboardRemove, Update)
 from telegram.ext import (CallbackContext, CallbackQueryHandler,
-                          CommandHandler, Filters, MessageHandler, Updater)
+                          CommandHandler, ConversationHandler, Filters,
+                          MessageHandler, Updater)
 
 from telebot.credentials import TOKEN, URL, bot_user_name, chat_id
 
@@ -12,43 +14,63 @@ updater = Updater(token=TOKEN)
 dispatcher = updater.dispatcher
 
 logging.basicConfig(
-  format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
 )
 logger = logging.getLogger(__name__)
 
+CHOICE, SEND = range(2)
 
-def start(update: Update, context: CallbackContext) -> None:
+
+def entry(update: Update, context: CallbackContext) -> None:
     keyboard = [[
         InlineKeyboardButton("Чат Мартына", callback_data='@martynomicon'),
         InlineKeyboardButton("Kode Frontenders", callback_data='@kode_frontend')
     ]]
-
-
     reply_markup = InlineKeyboardMarkup(keyboard)
+
+    context.user_data['message_text'] = update.message.text
+    context.user_data['chat_id'] = update.message.chat.id
+    context.user_data['message_id'] = update.message.message_id
 
     update.message.reply_text('Please choose:', reply_markup=reply_markup)
 
+    choice(Update, CallbackContext)
 
-def button(update: Update, context: CallbackContext) -> None:
-  query = update.callback_query
-
-  # CallbackQueries need to be answered, even if no notification to the user is needed
-  query.answer()
-
-  query.edit_message_text(text=f"Selected option: {query.data}")
-  print("🚀 ~ file: app.py ~ line 41 ~ update.message", update.message)
-
-  if update.message:
-    message = update.message
-
-    if message.chat.id != 129482161: # id of personal chat with bot
-      return 'ok'
+def choice(update: Update, context: CallbackContext) -> None:
+    query = update.callback_query
     
-    if message.text:
-      msg_id = message.message_id
-      from_chat_id = query.data
+    context.user_data['channel_id'] = query.data
 
-      updater.bot.forwardMessage(chat_id=chat_id, from_chat_id=from_chat_id, message_id=msg_id)
+    query.answer()
+    query.edit_message_text(text=f"Selected option: {query.data}")
+
+    send(Update, CallbackContext)
+
+
+def send(update: Update, context: CallbackContext) -> None:
+    user_data = context.user_data
+    # message_text = user_data.message_text
+    chat_id = user_data.chat_id
+    channel_id = user_data.channel_id
+    message_id = user_data.message_id
+
+    if chat_id != 129482161: # id of personal chat with bot
+        return done(Update, CallbackContext)
+    
+    updater.bot.forwardMessage(chat_id=chat_id, from_chat_id=channel_id, message_id=message_id)
+
+    return done(Update, CallbackContext)
+
+
+def done(update: Update, context: CallbackContext) -> int:
+    user_data = context.user_data
+
+    update.message.reply_text(
+        f"Until next time!"
+    )
+
+    user_data.clear()
+    return 
 
 def main():
   updater = Updater(TOKEN)
@@ -59,8 +81,7 @@ def main():
   botUrl = 'https://{URL}/{HOOK}'.format(URL=URL, HOOK=TOKEN)
   updater.bot.set_webhook(botUrl)
 
-  updater.dispatcher.add_handler(CommandHandler('start', start))
-  updater.dispatcher.add_handler(CallbackQueryHandler(button))
+  dispatcher.add_handler(MessageHandler(Filters.text, entry))
 
   # Run the bot until the user presses Ctrl-C or the process receives SIGINT,
   # SIGTERM or SIGABRT
