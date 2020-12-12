de64cc63a77c98224ae7fe4f2a28fc84b10a5887
Martyn <zogacc@gmail.com>
2020-12-12 20:51:33 +0200

    add chat

--- telebot/groups.py
+++ telebot/groups.py
@@ -1,5 +1,6 @@
 groups = {
   'Мартыномикон': '@martynomicon',
   'KODE frontenders': '-363265441', # kode frontenders
+  'Славный день': '-1001350551052',
   'Test chat': '-1001304984709', # test chat  
 }
