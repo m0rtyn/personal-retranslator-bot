61492ae682fcd0ed41534881ca124c2bf31a1ee1
Martyn <zogacc@gmail.com>
2020-12-15 23:12:31 +0200

    update deps and add flow for seamless conversation

--- app.py
+++ app.py
@@ -1,31 +1,36 @@
 import logging
 import os
 
-from telegram import (InlineKeyboardButton, InlineKeyboardMarkup,
-                      ReplyKeyboardRemove, Update)
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import (CallbackContext, CallbackQueryHandler,
                           CommandHandler, ConversationHandler, Filters,
                           MessageHandler, Updater)
 
-from telebot.credentials import CHAT_ID, TOKEN, URL
+from telebot.credentials import CHAT_ID, TEST_URL, TOKEN, URL
 from telebot.groups import groups
 
 logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
+    format='ℹ️  %(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
 )
 logger = logging.getLogger(__name__)
 
-# def toInlineKeyboard(str):
-#     return InlineKeyboardButton(str, callback_data=str)
+def splitArr(arr, size):
+    arrs = []
+    while len(arr) > size:
+        pice = arr[:size]
+        arrs.append(pice)
+        arr = arr[size:]
+    arrs.append(arr)
+    return arrs
 
-def fillKeyboard():
+def fillKeyboard(dict):
     result = []
-    for key, value in groups.items():
+    for key, value in dict.items():
         result.append(InlineKeyboardButton(key, callback_data=value))
 
-    return [result]
+    return splitArr(result, 2)
 
-keyboard = fillKeyboard()
+keyboard = fillKeyboard(groups)
 reply_markup = InlineKeyboardMarkup(keyboard)
 
 updater = Updater(TOKEN)
@@ -34,12 +39,6 @@ CHOICE, SEND, DONE = range(3)
 END = ConversationHandler.END
 
 def entry(update: Update, context: CallbackContext) -> None:
-    message = update.effective_message
-    context.user_data['message_text'] = message.text
-    context.user_data['chat_id'] = message.chat.id
-    context.user_data['message_id'] = message.message_id
-
-    # print(reply_markup)
     update.message.reply_text('Please choose:', reply_markup=reply_markup)
 
     return CHOICE
@@ -53,41 +52,32 @@ def choice(update: Update, context: CallbackContext) -> None:
     query.answer()
     query.edit_message_text(text=f"Selected option: {query.data}")
 
+    return SEND
+
+def send(update: Update, context: CallbackContext) -> None:
     user_data = context.user_data
-    chat_id = user_data['chat_id']
+    message = update.effective_message
+
+    chat_id = message.chat.id
+    message_id = message.message_id
     channel_id = user_data['channel_id']
-    message_id = user_data['message_id']
 
-    if chat_id != CHAT_ID: # id of personal chat with bot
-        print('🤷‍♂️', chat_id, CHAT_ID)
-        return
+    if chat_id == CHAT_ID: # id of personal chat with bot
+        update.message.reply_text('Гав')
+        updater.bot.forwardMessage(
+            chat_id=channel_id, from_chat_id=chat_id, message_id=message_id,
+            disable_notification=True
+        )
 
-    updater.bot.forward_message(
-        chat_id=channel_id, 
-        from_chat_id=chat_id, message_id=message_id, disable_notification=True
-    )
+        return SEND
 
-    return DONE
-
-# def send(update: Update, context: CallbackContext) -> None:
-#     print(context.user_data)
-#     user_data = context.user_data
-#     # message_text = user_data.message_text
-#     chat_id = user_data.chat_id
-#     channel_id = user_data.channel_id
-#     message_id = user_data.message_id
-
-#     if chat_id != 129482161: # id of personal chat with bot
-#         return 
-    
-#     updater.bot.forwardMessage(chat_id=chat_id, from_chat_id=channel_id, message_id=message_id)
-
-#     return 
-
+    if chat_id != CHAT_ID:
+        
+        return END
 
 def done(update: Update, context: CallbackContext) -> None:
     update.message.reply_text(
-        f"Until next time!"
+        f"woof woof!"
     )
 
     context.user_data.clear()
@@ -97,38 +87,36 @@ def done(update: Update, context: CallbackContext) -> None:
 
 def main() -> None:
     dispatcher = updater.dispatcher
-    PORT = int(os.environ.get('PORT', '8443'))
+    # PORT = int(os.environ.get('PORT', '8443'))
     updater.start_webhook(
-        listen="0.0.0.0",
-        port=PORT,
+        listen="localhost",
+        port=3000,
         url_path=TOKEN
     )
 
-    botUrl = 'https://{URL}/{HOOK}'.format(URL=URL, HOOK=TOKEN)
+    botUrl = 'https://{URL}/{HOOK}'.format(URL=TEST_URL, HOOK=TOKEN)
     updater.bot.set_webhook(botUrl)
 
     conv_handler = ConversationHandler(
-        entry_points=[MessageHandler(Filters.regex('^Пёс, .*'), entry)],
+        entry_points=[CommandHandler('woof', entry)],
         states={
             CHOICE: [
                 CallbackQueryHandler(choice),
             ],
-            # SEND: [
-            #     CallbackQueryHandler(send)
-            # ],
-            DONE: [
-                CommandHandler('done', done)
-            ]
+            SEND: [
+                CommandHandler('tyaf', done),
+                CommandHandler('woof', entry),
+                MessageHandler(Filters.all, send),
+            ],
         },
         fallbacks=[
-            CommandHandler('start', done),
-            MessageHandler(Filters.text, entry)
+            MessageHandler(Filters.all, done),
+            CommandHandler('woof', entry),
+            CommandHandler('tyaf', done),
         ],
     )
 
     dispatcher.add_handler(conv_handler)
-    # dispatcher.add_handler(MessageHandler(Filters.text, entry))
-    # dispatcher.add_handler(CallbackQueryHandler(choice))
 
     # Run the bot until the user presses Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT
