686a9a0abebab248e51c0b5bfbeebce0cfd83263
Martyn <zogacc@gmail.com>
2020-12-28 21:56:01 +0200

    update groups

--- telebot/groups.py
+++ telebot/groups.py
@@ -1,6 +1,7 @@
 groups = {
   'Мартыномикон': '@martynomicon',
-  'KODE frontenders': '-363265441', # kode frontenders
   'Славный день': '-1001350551052',
+  'Экстракоманд': '-1001126566190',
   'Test chat': '-1001304984709', # test chat  
+  'KODE frontenders': '-363265441', # kode frontenders
 }
