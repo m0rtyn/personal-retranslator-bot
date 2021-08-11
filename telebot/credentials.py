1bb123eece838c3cb9ad7715fa0c30b443269be4
Martyn <zogacc@gmail.com>
2021-08-11 08:56:44 +0200

    return credentials

---
+++ telebot/credentials.py
@@ -0,0 +1,5 @@
+TOKEN = "1427261129:AAEHCboM0MC81MI5qYv4t2lg6phVcBYXTL4"
+URL = "retranslator-bot.herokuapp.com"
+TEST_URL = "56d43b744bfe.ngrok.io"
+BOT_NAME = "martyn_retranslator_bot"
+CHAT_ID = 129482161
