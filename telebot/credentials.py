f4aeba1a0494c01c84ee70c25ea1ec30503c3743
Martyn <zogacc@gmail.com>
2022-01-10 16:24:35 +0200

    extract bot token to secret

--- telebot/credentials.py
+++ telebot/credentials.py
@@ -1,4 +1,8 @@
-TOKEN = "1427261129:AAEHCboM0MC81MI5qYv4t2lg6phVcBYXTL4"
+import os
+from dotenv import load_dotenv
+load_dotenv('../.env')
+
+TOKEN = os.getenv('TELEGRAM_BOT_TOKEN') 
 URL = "retranslator-bot.herokuapp.com"
 TEST_URL = "96b9-94-25-231-113.ngrok.io"
 BOT_NAME = "martyn_retranslator_bot"
